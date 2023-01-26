045eeb29ccb8a60fba93b69bd033e95ce6b567c3
Aleksandr <miranovich664@gmail.com>
2023-01-26 10:36:33 +0300

    first commit

---
+++ gulp/pug.js
@@ -0,0 +1,31 @@
+let path = require('./path/path.js')
+let pugbem = require('gulp-pugbem');
+let fs = require('fs')
+let emittyPug = null
+pugbem.m = '_';
+module.exports = function () {
+	if (!emittyPug) {
+		emittyPug = $.plugins.emitty.setup('src', 'pug', { makeVinylFile: true })
+	}
+
+	$.gulp.task('pug:build', () => {
+		return new Promise((resolve, reject) => {
+			emittyPug.scan(global.emittyPugChangedFile).then(() => {
+				$.gulp.src(path.path.src.pug)
+					.pipe($.plugins.plumber())
+					.pipe(emittyPug.filter(global.emittyPugChangedFile))
+					.pipe($.plugins.data(() => JSON.parse(fs.readFileSync('./src/base/data/data.json', 'utf8')))) // Парсим JSON
+					.pipe($.plugins.pug({
+						plugins: [pugbem],
+						pretty: !$.yargs.minifyHtml
+					}))
+					.pipe($.gulp.dest(path.path.build.root))
+					.on('end', () => {
+						$.browserSync.reload()
+						resolve()
+					})
+					.on('error', reject)
+			})
+		})
+	})
+}
